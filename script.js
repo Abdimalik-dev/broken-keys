979e2d4f390267ee4406fa51b0469386438d20b4
Abdimalik-dev <cabdixuseen076@gmail.com>
2023-11-26 22:58:21 +0300

    commit

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// Good Luck 💪🏾
+function brokenKeys(Right, Wrong) {
+    let brokenKeys = [];
+    for (let i = 0; i < Right.length; i++) {
+      if (Right[i] !== Wrong[i]) {
+        if (!brokenKeys.includes(Right[i])) {
+          brokenKeys.push(Right[i]);
+        }
+      }
+    }
+    return brokenKeys;
+  }
+  console.log(brokenKeys("fikrcamp bootcamp", "fikrcomp boorcamp")); 
+  console.log(brokenKeys("Hello Welcome", " Hjjlo Welcome"))
